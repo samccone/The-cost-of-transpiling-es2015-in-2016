6913c1ba9b0c48915f8487bc5a47a8ba4bb0c3b0
Adam Burgess <AdamBurgess@users.noreply.github.com>
2016-02-03 01:16:26 +0000

    Add JSPM

---
+++ jspm/jspm.config.js
@@ -0,0 +1,18 @@
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  globalEvaluationScope: false,
+  transpiler: "plugin-babel",
+  sourceMaps: false,
+  map: {
+    "plugin-babel": "npm:systemjs-plugin-babel@0.0.2"
+  },
+
+  packages: {
+    "src": {
+      defaultExtension: "js"
+    }
+  }
+});
