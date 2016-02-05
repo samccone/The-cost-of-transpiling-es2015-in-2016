7f4eec0e6515564c327eb030670cd4d06aae87f6
guybedford <guybedford@gmail.com>
2016-02-05 17:58:51 +0200

    update jspm case

--- jspm/jspm.config.js
+++ jspm/jspm.config.js
@@ -3,16 +3,16 @@ SystemJS.config({
     "npm:@*/*.json",
     "npm:*.json"
   ],
-  globalEvaluationScope: false,
   transpiler: "plugin-babel",
   sourceMaps: false,
+
   map: {
     "plugin-babel": "npm:systemjs-plugin-babel@0.0.2"
   },
 
   packages: {
     "src": {
-      defaultExtension: "js"
+      "defaultExtension": "js"
     }
   }
 });
